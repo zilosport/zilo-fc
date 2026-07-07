1db441159562f809e8424346e834b923587dd002
zelo-sport-fc <starling.t.official@gmail.com>
2026-07-07 22:54:00 +0300

    Delete canada.js

--- canada.js
+++
@@ -1,5 +0,0 @@
-const canadaClubs = [
-    { id: "toronto_fc", nameAr: "تورونتو إف سي", nameEn: "Toronto FC", countryFlag: "🇨🇦", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/8660.png", members: 0, points: 0, color: "linear-gradient(135deg, #e31837, #b3b2b2)" },
-    { id: "cf_montreal", nameAr: "سي إف مونتريال", nameEn: "CF Montreal", countryFlag: "🇨🇦", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/227563.png", members: 0, points: 0, color: "linear-gradient(135deg, #0033a0, #000000)" },
-    { id: "vancouver_whitecaps", nameAr: "فانكوفر وايتكابس", nameEn: "Vancouver Whitecaps", countryFlag: "🇨🇦", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/9752.png", members: 0, points: 0, color: "linear-gradient(135deg, #00245e, #8db9ca)" }
-];
