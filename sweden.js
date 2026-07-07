25bb4b5918518ab300269ff3f465bdd9a9538adc
zelo-sport-fc <starling.t.official@gmail.com>
2026-07-07 23:01:19 +0300

    Delete sweden.js

--- sweden.js
+++
@@ -1,5 +0,0 @@
-const swedenClubs = [
-    { id: "malmo_ff", nameAr: "مالمو", nameEn: "Malmo FF", countryFlag: "🇸🇪", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/8561.png", members: 0, points: 0, color: "linear-gradient(135deg, #6ca0dc, #ffffff)" },
-    { id: "aik", nameAr: "أيك سولنا", nameEn: "AIK", countryFlag: "🇸🇪", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/8562.png", members: 0, points: 0, color: "linear-gradient(135deg, #000000, #fcd116)" },
-    { id: "djurgarden", nameAr: "يورغوردينس", nameEn: "Djurgarden", countryFlag: "🇸🇪", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/8563.png", members: 0, points: 0, color: "linear-gradient(135deg, #005ca9, #fcd116)" }
-];
