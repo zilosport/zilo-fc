d44baab41685adbfe458de347ed64f96673d6c92
zelo-sport-fc <starling.t.official@gmail.com>
2026-07-07 23:00:49 +0300

    Delete portugal.js

--- portugal.js
+++
@@ -1,9 +0,0 @@
-const portugalClubs = [
-    { id: "benfica", nameAr: "بنفيكا", nameEn: "Benfica", countryFlag: "🇵🇹", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/9773.png", members: 0, points: 0, color: "linear-gradient(135deg, #ff0000, #ffffff)" },
-    { id: "porto", nameAr: "بورتو", nameEn: "Porto", countryFlag: "🇵🇹", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/9772.png", members: 0, points: 0, color: "linear-gradient(135deg, #00428c, #ffffff)" },
-    { id: "sporting_cp", nameAr: "سبورتينغ لشبونة", nameEn: "Sporting CP", countryFlag: "🇵🇹", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/9768.png", members: 0, points: 0, color: "linear-gradient(135deg, #008000, #ffffff)" },
-    { id: "braga", nameAr: "سبورتينغ براغا", nameEn: "SC Braga", countryFlag: "🇵🇹", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/9765.png", members: 0, points: 0, color: "linear-gradient(135deg, #d31c29, #ffffff)" },
-    { id: "vitoria_guimaraes", nameAr: "فيتوريا غيمارايش", nameEn: "Vitoria Guimaraes", countryFlag: "🇵🇹", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/9774.png", members: 0, points: 0, color: "linear-gradient(135deg, #000000, #ffffff)" },
-    { id: "boavista", nameAr: "بوافيشتا", nameEn: "Boavista", countryFlag: "🇵🇹", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/9766.png", members: 0, points: 0, color: "linear-gradient(135deg, #000000, #ffffff)" },
-    { id: "famalicao", nameAr: "فاماليساو", nameEn: "Famalicao", countryFlag: "🇵🇹", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/102128.png", members: 0, points: 0, color: "linear-gradient(135deg, #003b7a, #ffffff)" }
-];
