4f8e17fe70dda2af14224f1f3e01960ed115d858
zelo-sport-fc <starling.t.official@gmail.com>
2026-07-07 22:53:51 +0300

    Delete cameroon.js

--- cameroon.js
+++
@@ -1,4 +0,0 @@
-const cameroonClubs = [
-    { id: "coton_sport", nameAr: "القطن", nameEn: "Coton Sport", countryFlag: "🇨🇲", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10150.png", members: 0, points: 0, color: "linear-gradient(135deg, #009e60, #ffffff)" },
-    { id: "canon_yaounde", nameAr: "كانون ياوندي", nameEn: "Canon Yaounde", countryFlag: "🇨🇲", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10151.png", members: 0, points: 0, color: "linear-gradient(135deg, #e30613, #009e60)" }
-];
