26bb997f98fbfcf32342dd2bccec4c3f12dfbb24
zelo-sport-fc <starling.t.official@gmail.com>
2026-07-07 23:00:25 +0300

    Delete norway.js

--- norway.js
+++
@@ -1,5 +0,0 @@
-const norwayClubs = [
-    { id: "bodo_glimt", nameAr: "بودو غليمت", nameEn: "Bodo/Glimt", countryFlag: "🇳🇴", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/8559.png", members: 0, points: 0, color: "linear-gradient(135deg, #fcd116, #000000)" },
-    { id: "molde", nameAr: "مولده", nameEn: "Molde", countryFlag: "🇳🇴", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/8560.png", members: 0, points: 0, color: "linear-gradient(135deg, #005ca9, #ffffff)" },
-    { id: "rosenborg", nameAr: "روسنبورغ", nameEn: "Rosenborg", countryFlag: "🇳🇴", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/8561.png", members: 0, points: 0, color: "linear-gradient(135deg, #000000, #ffffff)" }
-];
