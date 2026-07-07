3e1c10387433495f848a4df9bc2352d1353304e7
zelo-sport-fc <starling.t.official@gmail.com>
2026-07-07 22:54:53 +0300

    Delete chile.js

--- chile.js
+++
@@ -1,5 +0,0 @@
-const chileClubs = [
-    { id: "colo_colo", nameAr: "كولو-كولو", nameEn: "Colo-Colo", countryFlag: "🇨🇱", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10090.png", members: 0, points: 0, color: "linear-gradient(135deg, #ffffff, #000000)" },
-    { id: "u_de_chile", nameAr: "يونيفرسيداد دي تشيلي", nameEn: "Universidad de Chile", countryFlag: "🇨🇱", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10091.png", members: 0, points: 0, color: "linear-gradient(135deg, #005ca9, #e30613)" },
-    { id: "u_catolica", nameAr: "يونيفرسيداد كاتوليكا", nameEn: "Universidad Catolica", countryFlag: "🇨🇱", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10092.png", members: 0, points: 0, color: "linear-gradient(135deg, #ffffff, #005ca9)" }
-];
