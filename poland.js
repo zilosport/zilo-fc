52032eee836eb49d86398d52f13fe464e93391c1
zelo-sport-fc <starling.t.official@gmail.com>
2026-07-07 23:03:29 +0300

    Delete poland.js

--- poland.js
+++
@@ -1,5 +0,0 @@
-const polandClubs = [
-    { id: "legia_warszawa", nameAr: "ليجيا وارسو", nameEn: "Legia Warszawa", countryFlag: "🇵🇱", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/8673.png", members: 0, points: 0, color: "linear-gradient(135deg, #ffffff, #005ca9)" },
-    { id: "lech_poznan", nameAr: "ليخ بوزنان", nameEn: "Lech Poznan", countryFlag: "🇵🇱", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/8675.png", members: 0, points: 0, color: "linear-gradient(135deg, #005ca9, #ffffff)" },
-    { id: "rakow", nameAr: "راكوف تشيستوخوفا", nameEn: "Rakow", countryFlag: "🇵🇱", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/192257.png", members: 0, points: 0, color: "linear-gradient(135deg, #e30613, #005ca9)" }
-];
