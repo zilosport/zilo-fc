40c6ec5b693a97c9d45388906daef3c0fef864ec
zelo-sport-fc <starling.t.official@gmail.com>
2026-07-07 23:02:21 +0300

    Delete uae.js

--- uae.js
+++
@@ -1,7 +0,0 @@
-const uaeClubs = [
-    { id: "al_ain", nameAr: "العين", nameEn: "Al Ain", countryFlag: "🇦🇪", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10161.png", members: 0, points: 0, color: "linear-gradient(135deg, #4b2383, #ffffff)" },
-    { id: "shabab_al_ahli", nameAr: "شباب الأهلي", nameEn: "Shabab Al Ahli", countryFlag: "🇦🇪", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10162.png", members: 0, points: 0, color: "linear-gradient(135deg, #e30613, #009e60)" },
-    { id: "al_jazira", nameAr: "الجزيرة", nameEn: "Al Jazira", countryFlag: "🇦🇪", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10163.png", members: 0, points: 0, color: "linear-gradient(135deg, #000000, #ffffff)" },
-    { id: "al_wahda", nameAr: "الوحدة", nameEn: "Al Wahda", countryFlag: "🇦🇪", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10164.png", members: 0, points: 0, color: "linear-gradient(135deg, #6c1d45, #ffffff)" },
-    { id: "sharjah", nameAr: "الشارقة", nameEn: "Sharjah", countryFlag: "🇦🇪", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10165.png", members: 0, points: 0, color: "linear-gradient(135deg, #ffffff, #e30613)" }
-];
