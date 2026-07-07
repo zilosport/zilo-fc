812e2260c30a0ce1c91c2e541c9664d6d900db3a
zelo-sport-fc <starling.t.official@gmail.com>
2026-07-07 23:03:08 +0300

    Delete senegal.js

--- senegal.js
+++
@@ -1,4 +0,0 @@
-const senegalClubs = [
-    { id: "generation_foot", nameAr: "جينيراسيون فوت", nameEn: "Generation Foot", countryFlag: "🇸🇳", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10140.png", members: 0, points: 0, color: "linear-gradient(135deg, #e30613, #009e60)" },
-    { id: "asc_diaraf", nameAr: "دياراف", nameEn: "ASC Diaraf", countryFlag: "🇸🇳", logo: "https://images.fotmob.com/image_resources/logo/teamlogo/10141.png", members: 0, points: 0, color: "linear-gradient(135deg, #009e60, #ffffff)" }
-];
